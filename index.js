d6e33fb86626edfdf186f8e9a5cbecfecc7b7cd1
unknown <ngovanthang1004@gmail.com>
2022-04-02 20:18:08 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+//back-to-top
+
+let backToTopBtn = document.querySelector('.back-to-top');
+window.onscroll = () => {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backToTopBtn.style.display = 'flex'
+    } else {
+        backToTopBtn.style.display = 'none'
+    }
+}
+
+// top nav menu
+let menuItems = document.getElementsByClassName('menu-item')
+
+Array.from(menuItems).forEach((item, index) => {
+    item.onclick = (e) => {
+        let currMenu = document.querySelector('.menu-item.active')
+        currMenu.classList.remove('active')
+        item.classList.add('active')
+    }
+})
